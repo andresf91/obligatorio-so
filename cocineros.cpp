c578bf4df0d26e47a9f0926ad5dd7e023e43da53
Andrés F <andresf91@gmail.com>
2023-12-06 07:59:48 -0300

    repostero, correcciones a cocineros

--- cocineros.cpp
+++ cocineros.cpp
@@ -2,7 +2,7 @@
 #include <semaphore.h>
 #include <pthread.h>
 
-sem_t cocinar_sem;
+sem_t cocinarSem;
 int platosDia = 0; // con cuántos platos comienzo el día
 int mostradorLibre = 27; // cuántos espacios libres hay en el mostrador
 
@@ -15,19 +15,19 @@ void *cocinero(void *arg) { // defino el proceso de cada cocinero, como puntero
                             // que necesito, para la suma de la siguiente instrucción
 
     while ((platosDia + id) < 180) { // mientras no corramos riesgo de pasarnos de 180 platos diarios
-        sem_wait(&cocinar_sem); // espero que el semáforo esté libre y lo tomo
+        sem_wait(&cocinarSem); // espero que el semáforo esté libre y lo tomo
         // Sección crítica
         if (mostradorLibre > 0){ // si hay lugar en el mostrador
             platosDia++; // aumento la cantidad de platos cocinados hoy
             mostradorLibre--; // disminuyo el espacio libre en el mostrador
         }
-        sem_post(&cocinar_sem);
+        sem_post(&cocinarSem);
     }
     pthread_exit(NULL); // salgo del thread
 }
 
 int main() {
-    sem_init(&cocinar_sem, 0, 1); // inicio el semáforo
+    sem_init(&cocinarSem, 0, 1); // inicio el semáforo
 
     int cocineros = 3, i; // límite de cocineros trabajando es 3. variable i es auxiliar
 
@@ -39,7 +39,7 @@ int main() {
         pthread_join(tid[i], NULL); // ejecuto el hilo
     }
 
-    sem_destroy(&cocinar_sem); // destruyo el semáforo
+    sem_close(&cocinarSem); // destruyo el semáforo
 
     return 0;
 }
--- repostero.cpp
+++ repostero.cpp
@@ -1,3 +1,46 @@
-/*
+#include <iostream>
+#include <semaphore.h>
+#include <pthread.h>
 
-*/
+int main() {
+
+    // para este no necesitamos threads porque es un solo repostero
+
+    sem_t hacerPostresSem;
+    int flanesHeladera=25; // postres en heladera
+    int check=flanesHeladera; // variable auxiliar de chequeo
+    int i;
+
+    sem_init(&hacerPostresSem, 0, 1); // inicio el semáforo
+
+    while(true){ // inicio "superbucle" para mantener proceso corriendo
+
+        while (check>0){
+            // mientras haya flanes en la heladera
+            // solamente actualizo la variable de chequeo
+            // para el próximo bucle
+
+            sem_wait(&hacerPostresSem); // espero que el semáforo esté libre y lo tomo
+            check=flanesHeladera;
+            sem_post(&hacerPostresSem); // libero el semáforo
+
+        }
+
+        // cuando se terminen los flanes en la heladera
+        // cocino 25 flanes
+
+        for (i=0;i<25;i++){
+            sem_wait(&hacerPostresSem); // espero que el semáforo esté libre y lo tomo
+            // preparo un flan
+            flanesHeladera++;
+            sem_post(&hacerPostresSem); // libero el semáforo
+        }
+
+        check=flanesHeladera; // actualizo variable de chequeo para el próximo superbucle
+
+    }
+
+    return 0;
+
+
+}
