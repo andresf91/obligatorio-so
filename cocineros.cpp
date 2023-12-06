2970f0f1addceb118bb884b9aa7b8c4c27da42ac
Andrés F <andresf91@gmail.com>
2023-12-06 00:13:09 -0300

    corrijo código cocineros faltaba chequeo de espacio en mostrador y otras cosas que me doy cuenta al momento de hacer el pseudocódigo como comentarios

--- cocineros.cpp
+++ cocineros.cpp
@@ -9,12 +9,18 @@ int mostradorLibre = 27; // cuántos espacios libres hay en el mostrador
 void *cocinero(void *arg) { // defino el proceso de cada cocinero, como puntero
 
     int id = *((int *)arg); // inicializo el entero id para poder usarlo desde el pthread_create
+                            // desreferencio el puntero, obteniendo el valor al que apunta
+                            // me costó bastante llegar a esta solución para generar
+                            // varios hilos, cada uno con el id del cocinero
+                            // que necesito, para la suma de la siguiente instrucción
 
     while ((platosDia + id) < 180) { // mientras no corramos riesgo de pasarnos de 180 platos diarios
         sem_wait(&cocinar_sem); // espero que el semáforo esté libre y lo tomo
         // Sección crítica
-        platosDia++; // aumento la cantidad de platos cocinados hoy
-        mostradorLibre--; // disminuyo el espacio libre en el mostrador
+        if (mostradorLibre > 0){ // si hay lugar en el mostrador
+            platosDia++; // aumento la cantidad de platos cocinados hoy
+            mostradorLibre--; // disminuyo el espacio libre en el mostrador
+        }
         sem_post(&cocinar_sem);
     }
     pthread_exit(NULL); // salgo del thread
@@ -25,14 +31,11 @@ int main() {
 
     int cocineros = 3, i; // límite de cocineros trabajando es 3. variable i es auxiliar
 
-    pthread_t tid[cocineros]; 
+    pthread_t tid[cocineros]; // array de identificadores de hilo
 
     for (i = 0; i < cocineros; i++) { // un hilo por cocinero
         int *id = new int(i); // para pasar el parámetro del id de cocinero
         pthread_create(&tid[i], NULL, cocinero, (void *)id); // creo el hilo
-    }
-
-    for (i = 0; i < cocineros; i++) {
         pthread_join(tid[i], NULL); // ejecuto el hilo
     }
 
